b499275c1c3f523c3abcacf1ae6e7a322255507b
Vince van Noort <vince.vannoort@student.hu.nl>
2017-06-02 23:05:48 +0200

    Added more dummy functions based on datasheet

--- fingerprintsensor/fingerprintsensor.hpp
+++ fingerprintsensor/fingerprintsensor.hpp
@@ -29,9 +29,26 @@ Communication functions
 */
 namespace Communication {
     namespace Commands {
+        int initialise();
         int send();
         int recieve();
         int execute();
+        int control_led();
+        int change_baud_rate();
+        int get_enrolled_fingerprint_count();
+        int check_enrollment_status();
+        int start_enrollment();
+        int enrollment();
+        int check_finger_pressing_status();
+        int delete_one_fingerprint();
+        int delete_all_fingerprints();
+        int verification_1_1();
+        int indentification_1_N();
+        int capture_fingerprint();
+        int make_template();
+        int set_security_level();
+        int get_security_level();
+        int terminate();
     }
     namespace Response {
     }
