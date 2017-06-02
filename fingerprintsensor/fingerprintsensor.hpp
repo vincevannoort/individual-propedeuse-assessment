0085a9f137f0df78abe362a8df33ef6c2a1dd97d
Vince van Noort <vince.vannoort@student.hu.nl>
2017-06-02 22:53:27 +0200

    Added sections, namespaces and some dummy functions

--- fingerprintsensor/fingerprintsensor.hpp
+++ fingerprintsensor/fingerprintsensor.hpp
@@ -9,12 +9,38 @@ using namespace std;
 Fingerprintsensor
 */
 class Fingerprintsensor {
+
+/*
+Packet variables
+*/
 protected:
     map< string, int > command_packet;
     map< string, int > response_packet;
     map< string, int > data_packet;
+
+/*
+Constructors
+*/
 public:
     Fingerprintsensor();
+
+/*
+Communication functions
+*/
+namespace Communication {
+    namespace Commands {
+        int send();
+        int recieve();
+        int execute();
+    }
+    namespace Response {
+    }
+    namespace Data {
+        int send();
+        int recieve();
+    }
+}
+
 };
 
 #endif
